87c3f2cc65831d335b671996a73254147db19dba
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-12-18 15:40:49 +0500

    learned usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-
+import { useState } from "react";
 
 function App() {
- 
+  const [counter, setCounter] = useState(15);
+
+  const add = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const remove = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
 
   return (
-  <div>
-    <h1>Hello</h1>
-  </div>
-  )
+    <div>
+      <h1>Hello</h1>
+
+      <button onClick={add}>
+        add {counter}
+      </button>
+      <br />
+
+      <button onClick={remove}>
+        remove {counter}
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
